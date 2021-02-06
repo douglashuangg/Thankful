5b4519f7f9f9c4d50e32c2f36fe036a1908cc83c
addison-ch <addisonchen.py@gmail.com>
2021-02-06 11:51:43 -0500

    fixed

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, request, redirect
 import requests, json, random
-from quotes.py import test1, test2
+from quotes import quote_api
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def landingpage():
-
-    return render_template('homepage.html', test1 = test1, test2 = test2)
-
+    return render_template("homepage.html", test1=quote_api()[0], test2=quote_api()[1])
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- quotes.py
+++ quotes.py
@@ -1,14 +1,17 @@
 import requests, json, random
 
+
 def quote_api():
-    #FOR QUOTES
+    # FOR QUOTES
     base_url = "https://goquotes-api.herokuapp.com/api/v1/"
     testing_response = requests.get(base_url + "/all/quotes").json()
-    quotes_list = testing_response['quotes']
-    authors = [name['author'] for name in quotes_list]
-    quote_names = [name['text'] for name in quotes_list]
+    quotes_list = testing_response["quotes"]
+    authors = [name["author"] for name in quotes_list]
+    quote_names = [name["text"] for name in quotes_list]
     test1 = random.choice(authors)
     test2 = random.choice(quote_names)
-    return test1, test2
+    l = [test1, test2]
+    return l
+
 
 print(quote_api())

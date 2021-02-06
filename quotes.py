d856f891ec1f02585d584e4549bb300668182c86
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2021-02-06 12:26:27 -0500

    inspirational quotes

--- quotes.py
+++ quotes.py
@@ -3,13 +3,13 @@ import requests, json, random
 
 def quote_api():
     # FOR QUOTES
-    base_url = "https://goquotes-api.herokuapp.com/api/v1/"
-    testing_response = requests.get(base_url + "/all/quotes").json()
-    quotes_list = testing_response["quotes"]
+    base_url = "https://type.fit/api/quotes"
+    quotes_list = requests.get(base_url).json()
     authors = [name["author"] for name in quotes_list]
-    quote_names = [name["text"] for name in quotes_list]
+    quote_text = [name["text"] for name in quotes_list]
     test1 = random.choice(authors)
-    test2 = random.choice(quote_names)
+    test2 = random.choice(quote_text)
+    print(test1, test2)
     l = [test1, test2]
     return l
 

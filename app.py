ddad1a350aca8401015490f2ec86975c72b1ecec
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2021-02-06 17:11:08 -0500

    database stuff

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import requests, json, random
 from functools import wraps
 import pymongo
 from quotes import quote_api
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
+
 app.secret_key = "verysecret123"
 # database
 client = pymongo.MongoClient("127.0.0.1", 27017)
@@ -22,6 +26,30 @@ def login_required(f):
             return redirect("/")
     return wrap
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+#database
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+    def __repr__(self): #recognize created new blogpost
+        return 'Blog post' + str(self.id)
+
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is content'
+    },
+    {
+        'title': 'Post 1',
+        'content': 'This is content'
+    }
+]
 
 @app.route("/")
 def landingpage():
@@ -53,10 +81,24 @@ def register():
     return render_template("signup.html")
 
 
-@app.route("/dashboard/")
+@app.route("/posts", methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template("dashboard.html")
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template("posts.html", posts=all_posts)
+        
+
+#@app.route("/dashboard/", methods=['POST'])
+#@login_required
+#def gratitudes(text):
 
 
 

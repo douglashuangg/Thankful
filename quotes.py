fe9d7c1c9f8e54dbf9c13f1ee8edf8236c484aff
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2021-02-08 08:50:11 -0500

    update quote sync with author

--- quotes.py
+++ quotes.py
@@ -1,16 +1,17 @@
 import requests, json, random
 
-
 def quote_api():
     # FOR QUOTES
+    new_dict = {}
     base_url = "https://type.fit/api/quotes"
     quotes_list = requests.get(base_url).json()
     authors = [name["author"] for name in quotes_list]
     quote_text = [name["text"] for name in quotes_list]
-    test1 = random.choice(authors)
-    test2 = random.choice(quote_text)
-    print(test1, test2)
-    l = [test1, test2]
+    for author, quote in zip(authors, quote_text):
+        new_dict.update({quote: author})
+    testing = random.choice(list(new_dict.items()))
+    test1 = ('"'+testing[0]+ '"' + "--"+testing[1]+'"')
+    l = [test1]
     return l
 
 
